0b4f1cfe25032a7d8a0c14870e0241f03511f77e
Jon Wright <jwright.taipei@gmail.com>
2020-04-15 12:57:02 +0800

    select_seqs now does 150 and 300, top and evenly distributed

--- select_seqs.py
+++ select_seqs.py
@@ -1,220 +1,223 @@
 #!/usr/bin/python3
 
-# This script read a target input (arg1) and a cdhit fasta output file (arg2)
-# it then does a pairwise alignment, it accepts alignmensts that are between 35-95% seqid with the target
-# and have a length 60% or over compared with with the target
-# It also checks if the same homologue is used twice and if there is an overlap of > 10% the shorter
-# sequence is rejected
-# It then prints out the top 150 hits, if there are less than 150 left it prints all of thosee too to accepted150.fasta
-# if there are more than 150 left then it picks 150 of those left-overs randomly with the top 150 to accepted300.fasta
+# This script reads a target input (arg1) and a cdhit fasta output file (arg2)
+# it then does a pairwise alignment, it accepts alignmensts that are between
+# 35-95% seqid with the target  and have a length 60% or over compared with
+# with the target
+# It also checks if the same homologue is used twice and if there is an overlap
+# of > 10% the shorter sequence is rejected
+# It then prints out the top 150 hits, if there are less than 150 it prints all
+# of thosee too to accepted150top.fasta
+# It then prints out the top 300 hits, if there are less than 300 it prints all
+# of thosee too to accepted300top.fasta
+# It then prints out an even spread of 150 hits to the accetped150sp.fasta file
+# It then prints out an even spread of 300 hits to the accetped300sp.fasta file
 
 # usage:
-# python3 select_seqs.py reference.fasta homologues.fasta
+# python3 select_seqs.py reference.fasta cdhit.out
 
-import os
 import sys
-import random
-import decimal
+import shutil
 from Bio import pairwise2
-from Bio.pairwise2 import format_alignment
 
 if len(sys.argv) != 3:
-    print ("Please give reference fasta file and the homologues fasta file.")
+    print("Please give reference fasta file and the homologues fasta file.")
     exit()
 
-TARGETFILE=open(sys.argv[1],"r")
-INFILE=open(sys.argv[2],"r")
-PREALIGN150=open("accepted150.fasta","w")
-PREALIGN300=open("accepted300.fasta","w")
-REJECT=open("rejected.fasta","w")
-NOTUSED=open("notchosen.fasta","w")
+TARGETFILE = open(sys.argv[1], "r")
+INFILE = open(sys.argv[2], "r")
+PREALIGN150TOP = open("accepted150top.fasta", "w")
+PREALIGN300TOP = open("accepted300top.fasta", "w")
+PREALIGN150SP = open("accepted150sp.fasta", "w")
+PREALIGN300SP = open("accepted300sp.fasta", "w")
+REJECTFILE = open("rejected.fasta", "w")
 
 # Intial setup is for 100000 sequences
-title       = []
-short_title = []
-sequence    = []
-reject      = []  # Done = Accepted and written, Reconsider = available for reconsideration, 
-                                              # A number other than 0 is the percentage for rejection
-                                              # Acceptable means it's not a reject
-index                = -1
-total_aligns_written =  0
-reconsider           =  0
-no_rejected          =  0
+TITLE = []
+SHORT_TITLE = []
+SEQUENCE = []
+REJECT = []  # A number other than 0 is the percentage for rejection
+             # Acceptable means it's not a reject
+INDEX = -1
 
-# Read in the target seqeunce to title[0] and sequence[0]
+# Read in the target seqeunce to TITLE[0] and SEQUENCE[0]
 for LINE in TARGETFILE:
     LINE = LINE.rstrip("\n") # remove the newline
     if LINE[0:1] == ">":
-        title.append(0)
-        short_title.append(0)
-        sequence.append("")
-        reject.append("acceptable")
-        index += 1    # increment the index
-        title[index]       = LINE
-        short_title[index] = LINE
+        TITLE.append(0)
+        SHORT_TITLE.append(0)
+        SEQUENCE.append("")
+        REJECT.append("Acceptable")
+        INDEX += 1    # increment the index
+        TITLE[INDEX] = LINE
+        SHORT_TITLE[INDEX] = LINE
     else:
-        sequence[index] = sequence[index] + LINE
+        SEQUENCE[INDEX] = SEQUENCE[INDEX] + LINE
 TARGETFILE.close()
-target_len = len(sequence[0])
+TARGET_LEN = len(SEQUENCE[0])
 
-# Now read in the cdhit output and save to title[1] ++ and sequence[1] ++ and short_title[1] ++
+# Now read in the cdhit output and save to TITLE[1] ++ and SEQUENCE[1] ++ and SHORT_TITLE[1] ++
 for LINE in INFILE:
     LINE = LINE.rstrip("\n") # remove the newline
     if LINE[0:1] == ">":
-        title.append(0)
-        short_title.append(0)
-        sequence.append("")
-        reject.append("Acceptable")
-        index += 1    # increment the index
-        title[index] = LINE
-        short_title[index], *junk = [x.strip() for x in LINE.split("/")]
+        TITLE.append(0)
+        SHORT_TITLE.append(0)
+        SEQUENCE.append("")
+        REJECT.append("Acceptable")
+        INDEX += 1    # increment the index
+        TITLE[INDEX] = LINE
+        SHORT_TITLE[INDEX], *junk = [x.strip() for x in LINE.split("/")]
     else:
-        sequence[index] = sequence[index] + LINE
+        SEQUENCE[INDEX] = SEQUENCE[INDEX] + LINE
 INFILE.close()
 
-# Now sort out the sequences we want to keep
-# Loop over the lot and remove those that don't meet the criteria 
-for i in range(1,index+1):
-    #if total_aligns_written < int(150):  # We only write 150 out maximum
-        alignments = pairwise2.align.globalxx(sequence[0], sequence[i])
-        score = alignments[0][2]
-        percentage = (score/target_len) * 100
-        overlap = (len(sequence[i] ) / target_len) *100
-        percentage_digits = round(percentage,2)
-        overlap_digits = round(overlap,2)
-        if percentage <= float(35):   # Reject not enough seqid
-            reject[i] = "Too Low: " + str(percentage_digits)
-            no_rejected += 1
-        elif percentage >= float(95): # Reject too much seqid
-            reject[i] = "Too High: " + str(percentage_digits)
-            no_rejected += 1
-        elif overlap <= float(60):    # Reject for lack of overlap
-            reject[i] = "Too Short: " + str(overlap_digits)
-            no_rejected += 1
+# Now start to sort out the sequences we want to keep
+# Loop over the lot and remove those that don't meet the criteria against the target sequence
+for i in range(1, INDEX+1):
+    alignments = pairwise2.align.globalxx(SEQUENCE[0], SEQUENCE[i])
+    score = alignments[0][2]
+    percentage = (score/TARGET_LEN) * 100
+    overlap = (len(SEQUENCE[i]) / TARGET_LEN) *100
+    percentage_digits = round(percentage, 2)
+    overlap_digits = round(overlap, 2)
+    if percentage <= float(35): # Reject not enough seqid
+        REJECT[i] = "Too Low: " + str(percentage_digits)
+    elif percentage >= float(95): # Reject too much seqid
+        REJECT[i] = "Too High: " + str(percentage_digits)
+    elif overlap <= float(60): # Reject for lack of overlap
+        REJECT[i] = "Too Short: " + str(overlap_digits)
 
-# At this point we need to check the short Uniref anmesm if there is a match with another
+# At this point we need to check the short Uniref names if there is a match with another
 # Get the two lenghts, align shorter with longer, get percentage
 # if > 10% we reject the shorter
 #>UniRef90_P13726/37-243 [subseq from] Tissue factor n=43 Tax=Catarrhini TaxID=9526 RepID=TF_HUMAN
-#>UniRef90_A0A2I3REM1/21-227 [subseq from] Coagulation factor III, tissue factor n=1 Tax=Pan troglodytes TaxID=9598 RepID=A0A2I3REM1_
+#>UniRef90_A0A2I3REM1/21-227 [subseq from] Coagulation factor III, tissue factor n=1
 # We use local algnment becasue we are looking for overgaps not a full-algnment
 # We set the mismatch and gap penalties to -10 because they would not exist in an overlap
 # percentage >= 10 means too much overkap so we reject the shorted chain
-for i in range(1,index+1):
-    for j in range(i+1,index+1):
-        if short_title[i] == short_title[j]:
-            length1=len(sequence[i])
-            length2=len(sequence[j])
+for i in range(1, INDEX+1):
+    for j in range(i+1, INDEX+1):
+        if SHORT_TITLE[i] == SHORT_TITLE[j]:
+            length1 = len(SEQUENCE[i])
+            length2 = len(SEQUENCE[j])
             # Now do the choosing only pick #2 is if is longer than #1
-            if length2 > length1: 
+            if length2 > length1:
                 longer = length2
                 shorter_seq = i
-                alignments = pairwise2.align.localms(sequence[i], sequence[j],1,-10,-10,-1)
+                alignments = pairwise2.align.localms(SEQUENCE[i], SEQUENCE[j], 1, -10, -10, -1)
             else:
                 longer = length1
                 shorter_seq = j
-                alignments = pairwise2.align.localms(sequence[j], sequence[i],1,-10,-10,-1)
+                alignments = pairwise2.align.localms(SEQUENCE[j], SEQUENCE[i], 1, -10, -10, -1)
             score = alignments[0][2]
             percentage = (score/longer) * 100
-            percentage_digits = round(percentage,2)
+            percentage_digits = round(percentage, 2)
             if percentage >= 10:
-                reject[shorter_seq] = "Overlaps with itself elsewhere: " + str(percentage_digits)
-                no_rejected += 1
+                REJECT[shorter_seq] = "Overlaps with itself elsewhere: " + str(percentage_digits)
+
+# At this point we need to create a new list of those that are acceptable
+# so we can loop over these later looking for whatever sets we want to make
+# we make this as a tuple
+MERGED_LIST_ORIG = tuple(zip(TITLE, SEQUENCE, REJECT))
+# List only Acceptable values
+MERGED_LIST_ACC = list(filter(lambda a: a[2] == "Acceptable", MERGED_LIST_ORIG))
+# List only non Acceptable values
+MERGED_LIST_REJ = list(filter(lambda a: a[2] != "Acceptable", MERGED_LIST_ORIG))
+REMAINING = len(MERGED_LIST_ACC)
+REJECTED = len(MERGED_LIST_REJ)
 
 # If there are not enough homologues, min of 5, we stop at this point
-number_left = index - no_rejected
-if number_left < 5:
-    print("Not enough homologues for conservation analysis: ",index)
+if REMAINING < 4:
+    print("Not enough homologues for conservation analysis: ", INDEX, REMAINING)
     exit()
 
-# Write out the target sequence first
-PREALIGN150.write(title[0])
-PREALIGN150.write("\n")
-PREALIGN150.write(sequence[0])
-PREALIGN150.write("\n")
-PREALIGN300.write(title[0])
-PREALIGN300.write("\n")
-PREALIGN300.write(sequence[0])
-PREALIGN300.write("\n")
+# Write out the target SEQUENCE first to each of the accepted files
+PREALIGN150TOP.write(MERGED_LIST_ACC[0][0])
+PREALIGN150TOP.write("\n")
+PREALIGN150TOP.write(MERGED_LIST_ACC[0][1])
+PREALIGN150TOP.write("\n")
+PREALIGN300TOP.write(MERGED_LIST_ACC[0][0])
+PREALIGN300TOP.write("\n")
+PREALIGN300TOP.write(MERGED_LIST_ACC[0][1])
+PREALIGN300TOP.write("\n")
+PREALIGN150SP.write(MERGED_LIST_ACC[0][0])
+PREALIGN150SP.write("\n")
+PREALIGN150SP.write(MERGED_LIST_ACC[0][1])
+PREALIGN150SP.write("\n")
+PREALIGN300SP.write(MERGED_LIST_ACC[0][0])
+PREALIGN300SP.write("\n")
+PREALIGN300SP.write(MERGED_LIST_ACC[0][1])
+PREALIGN300SP.write("\n")
 
-# Loop back over the sequences and start to print them out
-# We always do this and print out the top 150 acceptable sequencces - or until we run out of sequences
-for i in range(1,index+1):  
-    # if it's acceptable and we have written <= 150 so far write this one as well
-    if reject[i] == "Acceptable" and total_aligns_written < 150:
-        PREALIGN150.write(title[i])
-        PREALIGN150.write("\n")
-        PREALIGN150.write(sequence[i])
-        PREALIGN150.write("\n")
-        PREALIGN300.write(title[i])
-        PREALIGN300.write("\n")
-        PREALIGN300.write(sequence[i])
-        PREALIGN300.write("\n")
-        reject[i] = "Done"    # Mark this one as being DONE
-        total_aligns_written += 1
-    # If it's a reject write to the reject file and give a reason
-    elif reject[i] != "Acceptable":    
-        note = "REJECTED: reason is: " + str(reject[i])
-        REJECT.write(title[i])
-        REJECT.write("\n")
-        REJECT.write(note)
-        REJECT.write("\n")
-        REJECT.write(sequence[i])
-        REJECT.write("\n")
-    # If we have written >150 and this one is also acceptable mark it for later consideration
-    elif reject[i] == "Acceptable" and total_aligns_written >= 150:
-        reject[i] = "Reconsider"
-        reconsider += 1
-    else: # Anything else gets rejected - really should not see this though
-        note = "REJECTED: not sure why:" + str(reject[i])
-        REJECT.write(title[i])
-        REJECT.write("\n")
-        REJECT.write(note)
-        REJECT.write("\n")
-        REJECT.write(sequence[i])
-        REJECT.write("\n")
+# If there are < 150 remaining, write them all out
+if REMAINING <= 150:
+    for i in range(1, REMAINING):
+        PREALIGN150TOP.write(MERGED_LIST_ACC[i][0])
+        PREALIGN150TOP.write("\n")
+        PREALIGN150TOP.write(MERGED_LIST_ACC[i][1])
+        PREALIGN150TOP.write("\n")
+    # Close the other files and copy this file to all the other accepted files
+    PREALIGN150TOP.close()
+    PREALIGN150SP.close()
+    PREALIGN300TOP.close()
+    PREALIGN300SP.close()
+    shutil.copyfile("accepted150top.fasta", "accepted150sp.fasta")
+    shutil.copyfile("accepted150top.fasta", "accepted300top.fasta")
+    shutil.copyfile("accepted150top.fasta", "accepted300sp.fasta")
 
-# At this point we can close the 150 and 300 files
-PREALIGN150.close()
-REJECT.close()
+# Write out the top 150 file if there are more than 150 accepted homologue
+if REMAINING > 150:
+    for i in range(1, 151): # Write out the top 150
+        PREALIGN150TOP.write(MERGED_LIST_ACC[i][0])
+        PREALIGN150TOP.write("\n")
+        PREALIGN150TOP.write(MERGED_LIST_ACC[i][1])
+        PREALIGN150TOP.write("\n")
+    # Here we try to write out 150 that evenly sample the remaining homologues
+    INTERVAL = int(REMAINING /150) # Calculate the interval for the 150 samples
+    for i in range(1, REMAINING, INTERVAL):
+        PREALIGN150SP.write(MERGED_LIST_ACC[i][0])
+        PREALIGN150SP.write("\n")
+        PREALIGN150SP.write(MERGED_LIST_ACC[i][1])
+        PREALIGN150SP.write("\n")
+    PREALIGN150TOP.close()
+    PREALIGN150SP.close()
 
-# At this point we will randomly choose 150 from the reconsider group, unless there are < 150 of them
-# in which case we'll take them all
+# If the number of accepted homologues is between 150 and 300 we just write out all of them
+# and copy that to the 300 even spread file
+if 150 < REMAINING <= 300:
+    for i in range(1, REMAINING):
+        PREALIGN300TOP.write(MERGED_LIST_ACC[i][0])
+        PREALIGN300TOP.write("\n")
+        PREALIGN300TOP.write(MERGED_LIST_ACC[i][1])
+        PREALIGN300TOP.write("\n")
+    # Close the 300 even spread file and copy this file to it
+    PREALIGN300TOP.close()
+    PREALIGN300SP.close()
+    shutil.copyfile("accepted300top.fasta", "accepted300sp.fasta")
 
-if reconsider > 0 and reconsider <= 150:  # less than 150, but over 0 loop over the entire set looking for them
-    print("Using all")
-    for i in range(1,index+1):
-        if reject[i] == "Reconsider":
-            PREALIGN300.write(title[i])
-            PREALIGN300.write("\n")
-            PREALIGN300.write(sequence[i])
-            PREALIGN300.write("\n")
-            reject[i] = "Done"  # Mark this one as DONE
+# Write out the top 300 file if there are more than 300 accetped homologues
+if REMAINING > 300:
+    for i in range(1, 301):  # Write out the top 300
+        PREALIGN300TOP.write(MERGED_LIST_ACC[i][0])
+        PREALIGN300TOP.write("\n")
+        PREALIGN300TOP.write(MERGED_LIST_ACC[i][1])
+        PREALIGN300TOP.write("\n")
+    # Here we try to write out 300 that evenly sample the REMAINING homologues
+    INTERVAL = int(REMAINING / 300) # Calculate the interval for the 300 samples
+    for i in range(1, REMAINING, INTERVAL):
+        PREALIGN300SP.write(MERGED_LIST_ACC[i][0])
+        PREALIGN300SP.write("\n")
+        PREALIGN300SP.write(MERGED_LIST_ACC[i][1])
+        PREALIGN300SP.write("\n")
+    PREALIGN300TOP.close()
+    PREALIGN300SP.close()
 
-# If there are more than 150, then we will pick ones at random, check if they are Done, Reconsider or rejectss (a number != 0)
-# if reconsider we use them and then set them to done "Done"
-elif reconsider > 150 :
-    print("Reconsidering")
-    reconsider_loop = 0
-    while reconsider_loop < 150:
-        picked = random.randint(150,index) # we can start at 150
-        if reject[picked] == "Reconsider":
-            PREALIGN300.write(title[picked])
-            PREALIGN300.write("\n")
-            PREALIGN300.write(sequence[picked])
-            PREALIGN300.write("\n")
-            reject[picked] = "Done"  # Mark this one as DONE
-            reconsider_loop += 1
-
-# Close the 300 file
-PREALIGN300.close()
-
-# Now print out the not-choosen sequences
-for i in range(1,index+1):
-    if reject[i] == "Reconsider":
-        NOTUSED.write(title[i])
-        NOTUSED.write("\n")
-        NOTUSED.write(sequence[i])
-        NOTUSED.write("\n")
-# Close the notchosen file
-NOTUSED.close()
+# Loop back over the rejected sequences and print them out with a reason
+for i in range(0, REJECTED):
+    REJECTFILE.write(MERGED_LIST_REJ[i][0])
+    REJECTFILE.write("\n")
+    REJECTFILE.write(MERGED_LIST_REJ[i][2])
+    REJECTFILE.write("\n")
+    REJECTFILE.write(MERGED_LIST_REJ[i][1])
+    REJECTFILE.write("\n")
+REJECTFILE.close()
