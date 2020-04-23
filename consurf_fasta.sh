442afa67c02dbc7ed8e62a46a59f2492a093b78f
Jon Wright <jwright.taipei@gmail.com>
2020-04-23 23:02:20 +0800

    Added the r4s pdb and fasta files

--- consurf_fasta.sh
+++ consurf_fasta.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#########################################################################################################################
+###################################################################################################
 ## Jon Wright, IBMS, Academia Sinica, Taipei, 11529, Taiwan
 ## These files are licensed under the GLP ver 3, essentially you have the right
 ## to copy, modify and distribute this script but all modifications must be offered
 ## back to the original authors
-#########################################################################################################################
+###################################################################################################
 #
 # A simple script to replciate Consurf for a provided single chain fasta file
 
@@ -31,8 +31,9 @@ longtitle=`grep ^> $1`
 title="${longtitle:1}"
 
 # Remove output from previous runs
-/bin/rm -rf uniref90_list.txt prealignment.fasta postalignment.aln accepted.fasta uniref.tmp frequency.aln consurf_home.grades
-/bin/rm -rf homologues.fasta r4s_pdb.py initial.grades r4s.res prottest.out cdhit.log r4s.out frequency.txt
+/bin/rm -rf uniref90_list.txt prealignment.fasta postalignment.aln accepted.fasta uniref.tmp 
+/bin/rm -rf frequency.aln consurf_home.grades frequency.txt
+/bin/rm -rf homologues.fasta r4s_pdb.py initial.grades r4s.res prottest.out cdhit.log r4s.out
 
 # Jackhmmer the blast database looking for homologues
 echo "Jackhmmering the Uniref90 DB"
@@ -55,13 +56,16 @@ python3 ../$scripts/select_seqs.py $1.fasta cdhit.out
 
 # Use mapsci to produce an alignment
 echo "Aligning the final sequences"
-$mafftdir/bin/mafft-linsi --quiet --localpair --maxiterate 1000 --reorder --clustalout --namelength 30 ./accepted.fasta >| ./postalignment.aln
-$mafftdir/bin/mafft-linsi --quiet --localpair --maxiterate 1000 --reorder --namelength 30  ./accepted.fasta >| frequency.aln
+$mafftdir/bin/mafft-linsi --quiet --localpair --maxiterate 1000 --reorder --clustalout --namelength 30 \
+         ./accepted.fasta >| ./postalignment.aln
+$mafftdir/bin/mafft-linsi --quiet --localpair --maxiterate 1000 --reorder --namelength 30 \
+         ./accepted.fasta >| frequency.aln
 python3 ../$scripts/get_frequency.py frequency.aln >| frequency.txt
 
 # Get the best protein matrix
 echo "Running Prottest"
-java -jar $prottestdir/prottest-3.4.2.jar -i postalignment.aln -JTT -LG -MtREV -Dayhoff -WAG -CpREV -S 1 -threads 2 >| prottest.out
+java -jar $prottestdir/prottest-3.4.2.jar -i postalignment.aln -JTT -LG -MtREV -Dayhoff -WAG \
+        -CpREV -S 1 -threads 2 >| prottest.out
 best_model=`grep 'Best model according to BIC:' prottest.out | awk  '{print $6}'`
 if [ "$best_model" == "JTT" ] ; then
     rate_model="-Mj"
@@ -81,8 +85,9 @@ fi
 
 # Run the rate4site to ge the sconsurf scores
 echo "Running rate4site and grading the scores"
-$rate4sitedir/rate4site_doublerep -ib -a "$title" -s ./postalignment.aln -zn $rate_model -bn -l ./r4s.log -o ./r4s.res  -x r4s.txt >| r4s.out
+$rate4sitedir/rate4site_doublerep -ib -a "$title" -s ./postalignment.aln -zn $rate_model -bn \
+     -l ./r4s.log -o ./r4s.res  -x r4s.txt >| r4s.out
 
 # Turn those scores into grades
-PYTHONPATH=. python3 ../$scripts/r4s_to_grades.py r4s.res initial.grades
+PYTHONPATH=. python3 ../$scripts/r4s_to_grades_fasta.py r4s.res initial.grades
 paste initial.grades frequency.txt > consurf_home.grades
