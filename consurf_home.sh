12f67774b611284de2b668decef0431254a09721
Jon Wright <jwright.taipei@gmail.com>
2020-09-19 15:08:27 +0800

    Allows use of single cpu for jackhmmer

--- consurf_home.sh
+++ consurf_home.sh
@@ -58,7 +58,7 @@ fi
 
 # Jackhmmer the blast database looking for homologs
 echo "Jackhmmering the Uniref90 DB"
-$hmmerdir/binaries/jackhmmer -E 0.0001 --domE 0.0001 --incE 0.0001 -N 1 \
+$hmmerdir/binaries/jackhmmer -E 0.0001 --domE 0.0001 --incE 0.0001 -N 1 --cpu $threads \
         -o cons_hmmer.out -A uniref90_list.txt cons.fasta $dbdir/uniref90.fasta
 
 # Remove the PDB_ATOM / given fasta entry - probably not needed but good to do anyway
